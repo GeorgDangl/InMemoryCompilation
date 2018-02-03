4335a42b73c2dd047c8a5955ee655704f7aa4837
Georg Dangl <georg@dangl.me>
2018-02-03 16:10:16 +0100

    Clean

--- test/InMemoryCompilation.Tests/Properties/AssemblyInfo.cs
+++ test/InMemoryCompilation.Tests/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InMemoryCompilation.Tests")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
